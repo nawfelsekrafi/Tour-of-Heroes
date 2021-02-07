3ad8bf0366d2eef7c91c06eb0cbd51b4e217e8ee
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2021-02-07 07:59:48 +0000

    third commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero ={
-    id:1,
-    name:'Khalid ibn Al-Walid'
-  };
-  heroes = HEROES;
-  constructor() { }
+  heroes : Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
-  selectedHero: Hero;
-  onSelect(hero: Hero):void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes=>
+      this.heroes = heroes );
   }
 }
